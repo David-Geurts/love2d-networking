1514e71df494e380209fb429663160836a54f52d
John Redmond <redmondjohnf@gmail.com>
2014-08-10 17:34:40 +0930

    added initial files

---
+++ ClientTest/main.lua
@@ -0,0 +1,25 @@
+-- Client test
+
+require "enet"
+
+function love.load()
+	host = enet.host_create()
+	server = host:connect("localhost:6789")
+end
+
+function love.update( dt )
+	local event = host:service(100)
+	if event then
+		if event.type == "connect" then
+			print("Connected to", event.peer)
+			event.peer:send("hello world")
+		elseif event.type == "receive" then
+			print("Got message: ", event.data, event.peer)
+			love.event.quit()
+		end
+	end
+end
+
+function love.draw()
+	love.graphics.print("Hello World!", 400, 300)
+end
---
+++ ServerTest/main.lua
@@ -0,0 +1,19 @@
+-- Server test
+
+require "enet"
+
+function love.load()
+	host = enet.host_create("localhost:6789")
+end
+
+function love.update( dt )
+	local event = host:service(100)
+	if event and event.type == "receive" then
+		print("Got message: ", event.data, event.peer)
+		event.peer:send(event.data)
+	end
+end
+
+function love.draw()
+	love.graphics.print("Hello World!", 400, 300)
+end
